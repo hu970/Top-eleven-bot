42343bfdbf01b6df009e84d7dd9677d65d663043
hu970 <chaumiothugo@gmail.com>
2025-07-29 10:44:33 +0200

    Correction bot token

--- main.py
+++ main.py
@@ -2,7 +2,7 @@ import os
 from telegram import Update, InlineKeyboardButton, InlineKeyboardMarkup
 from telegram.ext import ApplicationBuilder, CommandHandler, ContextTypes, CallbackQueryHandler
 
-TOKEN = os.environ.get("BOT TOKEN")
+TOKEN = os.environ.get("BOT_TOKEN")
 
 async def start(update: Update, context: ContextTypes.DEFAULT_TYPE):
     await update.message.reply_text(
